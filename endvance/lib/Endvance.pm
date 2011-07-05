bf76ec4b9818cf2fa3a5d047c6d98d54d5884510
Peter Vereshagin <peter@vereshagin.org>
2011-07-05 11:49:01 +0400

    http://bugs.box/show_bug.cgi?id=117 docs init

--- endvance/lib/Endvance.pm
+++ endvance/lib/Endvance.pm
@@ -22,8 +22,13 @@ use DBI;
 use Endvance::Base;
 use Endvance::Parser;
 
+our $VERSION = '0.01';
+
 # Default commands for VCS
-const my @GIT => [qw/git add */], [qw/git commit -a -m/];
+const my @GIT => (
+    ['/usr/local/bin/git' => qw/add */],
+    ['/usr/local/bin/git' => qw/commit -a -m/],
+);
 
 # Comment the commit with strftime();
 const my $COMMENT => 'Endvance backed up this at %F %T';
@@ -243,3 +248,213 @@ sub vcs_cmd {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+C<Endvance> - backup mariadb/mysql databases to Git VCS/SCM
+
+=head1 VERSION
+
+Initial version 0.01 is described here.
+
+=head1 SYNOPSIS
+
+=head2 Binary script supplied
+
+Suppose your archive or snapshot is called endvance.tar.gz and you have placed
+it into the separate directory. Extract the archive:
+
+    tar -zxvf endvance.tar.gz
+
+Next you have to decide where to keep configs and backups I assume them all by
+default here. Change working directory to a configuration one:
+
+    cd endvance/etc
+
+Take the sample config file and copy it to the working one:
+
+    cp endvance.sample.json endvance.json
+
+Edit the file according by your needs, at the least mysql credentials at the
+very begin.
+
+After that, change to the directory where to store the backups, you can set
+the different one as C<"dir"> in the C<"config"> section of a config file:
+
+    cd ../var
+
+Initialize the git repository there:
+
+    git init
+
+This will be the repository to keep your dumps.
+
+Run the C<Endvance> then:
+
+    ../bin/endvance
+
+As its first optional argument, C<Endvance> accepts the configuration file name.
+
+It should finish with the commit output.
+
+=head2 Perl module supplied
+
+In procedural orientation:
+
+    use Endvance;
+    Endvance::perform();
+
+Object-oriented:
+
+    use Endvance;
+    Endvance->perform;
+
+More object-oriented:
+
+    use Endvance;
+
+    my $endvance = Endvance->new;
+    $endvance->perform;
+
+Even more object-oriented:
+
+    ### Should be in MyBackup.pm
+    package MyBackup;
+
+    use base 'Endvance';
+
+    # Some subs are overrideable here, 'vcs_cmd' and 'configure' for instance
+    # They let you to change the VCS used and a configuration format/filename
+
+    1;
+
+    ### MAIN
+    package main;
+    use MyBackup;
+
+    my $backup = MyBackup->new;
+    $backup->perform;
+
+    1;
+
+=head1 DESCRIPTION
+
+Database backup is a complicated task when it needs to meet some key
+requirements like the tight storage and an ability to browse changes. Those
+can be met by mean of a version control system appliance but there are
+circumstances to trial over in the case if you track changes between sql
+dumps.
+
+Main of them is the C<timestamp> columns in the tables and that is why I
+needed to make C<Endvance> for myself.
+
+Those C<timestamp> database fields are not necessarily to be defined as a
+C<sql> C<timestamp> columns but can carry out the same meaning. In my instance
+this is a column in the C<IndexedSearch_docInfo> table keeping information
+about the documents indexed for the full text search by mean of
+L<DBIx::FullTextSearch> module used in L<http://WebGUI.org> content management
+system (C<CMS>). Such a column is defined as:
+
+    `dateIndexed` int(11) NOT NULL DEFAULT '0',
+
+That's it: you can omit the timestamp field if you prefer C<sql> dumps without
+columns' names in the C<INSERT> (or C<REPLACE>) statements and you can pass a
+C<null> to it in the other case but this one is definitely the case to insert
+the predefined content, the C<UNIX_TIMESTAMP()> C<sql> function.
+
+For every other case the C<mysqldump> is taken as it is without any changes.
+You can safely omit the databases and/or tables and/or fields that should be
+backed up to C<Git> as they appear on a dump.
+
+=head1 USAGE
+
+From your command prompt, run:
+
+    /path/to/your/endvance [</the/other/path/config.json>]
+
+C<Endvance> is designed to be used as a regular cron job. Put it like this to
+your C<crontab>:
+
+    5 3 * * * /path/to/endvance  /config/path/config1.json
+    # Another server or dir or other configured thing
+    5 4 * * * /path/to/endvance  /config/path/config2.json
+
+Care should be taken about configuration file, C<etc/endvance.json> by
+default. This is a file in C<JSON> format, described like this:
+
+    // Hash refernce to be Endvance's conf
+    {
+
+    // Constant info of the environment
+    "config":{
+
+        // Database settings
+        "db":{
+            "dsn":"dbi:mysql:mysql_enable_utf8=1;",
+
+            // "socket":"/path/to/mysql.sock" can be passed instead of "host"
+            "host":"127.0.0.1",
+            "user":"endvance",
+            "pass":"ecnavdne",
+
+            // additional attributes for DBI->connect()
+            "attrs":{
+                "mysql_auto_reconnect":1
+            }
+        },
+
+        // Dump command, no escaping but JSON's is needed
+        // Substitutions are made: %u for user, %p for password
+        // host/socket and a '-d' for no table's data needed are added
+        // at the begin of the command
+        // Database and a table name are added at the end
+        // "-c" stands for columns names inclusion in the dump here.
+        "dump":[
+            "/usr/local/bin/mysqldump", "-u", "%u", "--opt", "--skip-extended-insert",
+            "--skip-dump-date", "--skip-comments",
+            "--skip-quick", "--skip-lock-tables", "--create-options", "-c", "-p%p"
+        ]
+    },
+
+    // Constant info about the databases
+    "bases":{
+
+        // For the typical WebGUI database...
+        "webgui":{
+
+            // in its "IndexedSearch_docInfo" table ...
+            "IndexedSearch_docInfo":{
+
+                // for its "dateIndexed" column ...
+                "dateIndexed":{
+
+                    // change its value to the sql statement
+                    // any constant should be enclosed in '' here
+                    "value":"unix_timestamp()"
+                }
+            }
+        },
+
+        // For the typical Skybill (http://skybill.sf.net) database ...
+        "skybill":{
+
+            // skip all those tables' data as they are changed too frequently
+            "raw":1,
+            "clients":1,
+            "servers":1,
+            "details_daily":1
+        },
+
+        // These databases are not needed (I think) to backup
+        "information_schema":1,
+        "performance_schema":1
+    }
+    }
+
+
+
+=cut
